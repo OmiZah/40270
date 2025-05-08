61b6113e565ded0333a102652271557de4324d24
OmiZah <ozdiazm@hotmail.com>
2025-05-08 16:54:17 -0600

    09-10 modulos

---
+++ html00/x.js
@@ -0,0 +1,11 @@
+function x(params) {
+    y("x")
+    console.log("x: " + params)
+    return "fin x"   
+}
+
+function xx(params) {
+    y("xx")
+    console.log("xx: " + params)
+    return "fin xx"   
+}
